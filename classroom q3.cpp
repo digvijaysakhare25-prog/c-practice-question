380c47e30868d0b27ae31c5b318f24b822d7491b
digvijaysakhare25-prog <digvijaysakhare25@gmail.com>
2026-09-25 07:45:18 +0530

    Add files via upload

---
+++ classroom q3.cpp
@@ -0,0 +1,16 @@
+//to print the no is even or odd
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter your no: ";
+cin>>n;
+if(n%2==0){
+cout<<n<<"  is even no"<<endl;
+}
+else{
+cout<<n<<" is odd no"<<endl;
+}
+return 0;
+
+}
