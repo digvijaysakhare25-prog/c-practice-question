9de0c28276cff7410d89482dab14516b9d023ec8
digvijaysakhare25-prog <digvijaysakhare25@gmail.com>
2026-09-25 08:17:45 +0530

    Add files via upload

---
+++ classroom q7.cpp
@@ -0,0 +1,14 @@
+//to print 1 to 10 no. by do while loop
+#include<iostream>
+using namespace std;
+int main(){
+int n=1;
+do {
+    cout<<n<<endl;
+    n++;
+}
+while (n<=10);
+    return 0;
+}
+
+
