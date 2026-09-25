c94a498d979ab11e4308e3e7f02cc947d5fdffd6
digvijaysakhare25-prog <digvijaysakhare25@gmail.com>
2026-09-25 08:00:12 +0530

    Add files via upload

---
+++ classroom q4.cpp
@@ -0,0 +1,13 @@
+//to print the addition of two no by taking no from user
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,sum;
+    cout<<"enter num 1 : ";
+    cin>> num1 ;
+    cout<<"enter num 2 : ";
+    cin>> num2 ;
+    sum=num1+num2;
+    cout<<"sum of "<<num1<<" and "<<num2<<" is = "<<sum<<endl;
+return 0;
+}
