a3f018a23257f868658eab28ec1b6555e2eb5a6b
digvijaysakhare25-prog <digvijaysakhare25@gmail.com>
2026-09-25 07:46:14 +0530

    Update classroom q3.cpp

--- classroom q3.cpp
+++ classroom q3.cpp
@@ -2,15 +2,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n;
-cout<<"enter your no: ";
-cin>>n;
+  int n;
+  cout<<"enter your no: ";
+  cin>>n;
 if(n%2==0){
-cout<<n<<"  is even no"<<endl;
+  cout<<n<<"  is even no"<<endl;
 }
 else{
-cout<<n<<" is odd no"<<endl;
+  cout<<n<<" is odd no"<<endl;
 }
 return 0;
-
 }
